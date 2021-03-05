4bbd5c25f2187562f90e4cc8a21e6904a72fa979
Vladimir Belyayev <vladimirs.belajevs@gmail.com>
2021-03-05 11:54:16 +0200

    Fixed ExpressionType.Convert SqlVisitor issue.
    - The values of ExpressionType.Convert are already converted on .NET when parsing expression tree, we don't need to pass conversion operations to SQL

---
+++ Lambdy.Tests/Casting/CastingTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Lambdy.Tests.TestModels.NorthwindTables;
+using Xunit;
+
+namespace Lambdy.Tests.Casting
+{
+    public class CastingTest
+    {
+        [Fact]
+        public void WhereInputCastShouldWork()
+        {
+            var orderId = 222;
+            
+            Action act = () =>
+            {
+                LambdyQuery
+                    .ByModel(new
+                    {
+                        Table = (OrderDetail) null
+                    })
+                    .Where(x => x.Table.OrderId == orderId)
+                    .Compile();
+            };
+            
+            act
+                .Should()
+                .NotThrow<Exception>();
+        }
+    }
+}
