f2608cc5ee0ebfe0d781f2876068d02a7bd99ad9
Vladimir Belyayev <vladimirs.belajevs@gmail.com>
2021-03-11 10:39:40 +0200

    Fixed typed select clause
    - Introduced member init expression resolver to parse non-anonymous class object construction

--- Lambdy.Tests/Select/SelectOperationTest.cs
+++ Lambdy.Tests/Select/SelectOperationTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FluentAssertions;
 using Lambdy.Tests.Constants;
+using Lambdy.Tests.Select.Models;
 using Lambdy.Tests.TestModels.Tables;
 using Xunit;
 
@@ -9,7 +10,7 @@ namespace Lambdy.Tests.Select
     public class SelectOperationTest
     {
         [Fact]
-        public void SelectShouldBeCreated()
+        public void AnonymousSelectShouldBeCreated()
         {
             var expectedResult = $"{SqlClauses.Select} Table.Name AS AliasName,{LambdyRegex.NewLineOrWhitespace}" +
                                  $"Table.Surname AS AliasSurname,{LambdyRegex.NewLineOrWhitespace}" +
@@ -36,6 +37,64 @@ namespace Lambdy.Tests.Select
                 .Should()
                 .MatchRegex(expectedResult);
 
+        }
+        
+        [Fact]
+        public void TypedSelectShouldBeCreated()
+        {
+            var expectedResult = $"{SqlClauses.Select} Table.Name AS AliasName,{LambdyRegex.NewLineOrWhitespace}" +
+                                 $"Table.Surname AS AliasSurname,{LambdyRegex.NewLineOrWhitespace}" +
+                                 "Table2.AddressLine2 AS AddressLine2";
+            
+            var sqlResult = LambdyQuery
+                .ByModel(new
+                {
+                    Table = (Person) null,
+                    Table2 = (Address) null,
+                })
+                .WithTemplate($"{LambdyTemplateTokens.Select} " +
+                              $"{SqlClauses.From} {nameof(Person)} Table")
+                .Where(x => x.Table.Id == Guid.Empty)
+                .Select(x => new SimplePersonResult
+                {
+                    AliasName = x.Table.Name,
+                    AliasSurname = x.Table.Surname,
+                    AddressLine2 = x.Table2.AddressLine2
+                })
+                .Compile();
+
+            sqlResult.Sql
+                .Should()
+                .MatchRegex(expectedResult);
+
+        }
+        
+        [Fact]
+        public void SelectByClassShouldNotThrowException()
+        {
+            Action act = () =>
+            {
+                LambdyQuery
+                    .ByModel(new
+                    {
+                        Table = (Person) null,
+                        Table2 = (Address) null,
+                    })
+                    .WithTemplate($"{LambdyTemplateTokens.Select} " +
+                                  $"{SqlClauses.From} {nameof(Person)} Table")
+                    .Where(x => x.Table.Id == Guid.Empty)
+                    .Select(x => new SimplePersonResult
+                    {
+                        AliasName = x.Table.Name,
+                        AliasSurname = x.Table.Surname,
+                        AddressLine2 = x.Table2.AddressLine2
+                    })
+                    .Compile();
+            };
+            
+            act
+                .Should()
+                .NotThrow<Exception>();
         }
     }
 }
