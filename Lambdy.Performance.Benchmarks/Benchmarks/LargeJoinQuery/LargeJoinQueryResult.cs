d9811e7ceff0f4fbb5e8fda777e05099b67df965
Vladimir Belyayev <vladimirs.belajevs@gmail.com>
2021-04-01 12:59:44 +0300

    Added new benchmark

---
+++ Lambdy.Performance.Benchmarks/Benchmarks/LargeJoinQuery/LargeJoinQueryResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Lambdy.Performance.Benchmarks.Benchmarks.LargeJoinQuery
+{
+    public class LargeJoinQueryResult
+    {
+        public long ProductId { get; set; }
+        
+        public string OrderDetailId { get; set; }
+        
+        public long OrderId { get; set; }
+        
+        public long SupplierId { get; set; }
+        
+        public long CategoryId { get; set; }
+        
+        public long EmployeeId { get; set; }
+        
+        public string EmployeeTerritoryId { get; set; }
+        
+        public string TerritoryId { get; set; }
+        
+        public string CustomerId { get; set; }
+    }
+}
